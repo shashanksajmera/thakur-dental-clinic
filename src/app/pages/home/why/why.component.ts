c83bd765b55feccc7d30ea232a7fcfeb8d0892f6
Shashank Ajmera <shashanksajmera@gmail.com>
2023-04-09 19:29:06 +0530

    Why our clinic added to home page

--- src/app/pages/home/why/why.component.ts
+++ src/app/pages/home/why/why.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./why.component.css']
 })
 export class WhyComponent {
-
+  why_cards = [
+    {
+      title:'Highly Trained and Qualified Dental Team',readmore:''
+  },{
+    title:'Advanced Dental Treatment Procedures and Equipments',readmore:''
+},{
+  title:'Extensive Range of Dental Services',readmore:''
+},{
+  title:'Visible Results and Radiant Smiles',readmore:''
+},];
+  why_card = 'card2';
 }
